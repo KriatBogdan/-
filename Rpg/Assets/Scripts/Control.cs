da28684e76a5749477cac0b2209e30fa9c4b856d
DIDjeiROK <artem.pogl@gmail.com>
2019-10-29 22:16:53 +0200

    Добавил бег и шаг

--- Rpg/Assets/Scripts/Control.cs
+++ Rpg/Assets/Scripts/Control.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
 	float Ver, Hor, Jump;
-	float speed = 5;
+	float Speed;
 	float jumpSpeed = 100;
 	bool isGround;
+	public float RunSpeed = 15, StepSpeed = 3, NormalSpeed = 7;  
     // Start is called before the first frame update
     void Start()
     {
@@ -33,10 +34,22 @@ public class Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    	if (Input.GetKey(KeyCode.LeftShift))
+    	{
+    		Speed = RunSpeed;
+    	}
+    	else if (Input.GetKey(KeyCode.LeftControl))
+    	{
+    		Speed = StepSpeed;
+    	} 
+    	else
+    	{
+    		Speed = NormalSpeed;
+    	} 
     	if(isGround)
     	{
-    		Ver = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        	Hor = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+    		Ver = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+        	Hor = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
         	Jump = Input.GetAxis("Jump") * Time.deltaTime * jumpSpeed;
         	GetComponent<Rigidbody>().AddForce(transform.up * Jump, ForceMode.Impulse);
     	}
